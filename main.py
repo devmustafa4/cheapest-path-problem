347af9356e9ae6af1bbf41efaa855ffceadeaf65
Muhammad Mustafa <devmustafa4@gmail.com>
2024-10-23 22:42:38 +0500

    Prompt user for start and end airports

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from utils import find_cheapest_path, get_data_from_path
 
 airports = get_data_from_path('data/data.json')
-start_airport = 'ISB'
-end_airport = 'NYC'
+
+start_airport = input('Enter the start airport: ').upper()
+end_airport = input('Enter the end airport: ').upper()
+
 path, cost = find_cheapest_path(airports, start_airport, end_airport)
-# Expected output: ['ISB', 'CBS', 'NYC'], 1350
-print(f'Start: {start_airport}', f'End: {end_airport}', f'Path: {path}', f'Cost: {cost}', sep='\n')
+print(f'\nPath: {path}', f'Cost: {cost}', sep='\n')
